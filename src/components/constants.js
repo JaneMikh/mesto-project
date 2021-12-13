20b1eb26ec7c5afaf550069a57f6e0558793b0fc
Evgeniya Mikhaylyuk <88273125+JaneMikh@users.noreply.github.com>
2021-12-13 22:37:54 +0300

    fix: добавить util функцию

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,5 +1,3 @@
-export {config};
-
 const config = {
   formSelector: '.form',
   inputSelector: '.form__input',
@@ -8,34 +6,83 @@ const config = {
   inactiveButtonClass: 'form__button-submit_inactive',
 };
 
-//Массив, состоящий из карточек, задаваемых по умолчанию
-/*const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+const popupList = document.querySelectorAll('.popup');
+//Попар для редактрования профиля
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+const buttonEditForm = document.querySelector('.user-profile__button-edit');
+const buttonClosePopupEditForm = popupEditProfile.querySelector('.popup__button-close_type_profile');
+const popupProfileForm = document.querySelector('.form_type_profile');
+const nameInput = popupProfileForm.querySelector('#name');
+const jobInput = popupProfileForm.querySelector('#aboutyourself');
+const buttonSubmitProfile = popupProfileForm.querySelector('.form__button-submit_type_profile');
 
-// Перевернутый массив
-const initialCardsReverse = initialCards.reverse();
-*/
+//Данные пользователя на главной странице
+const userProfileName = document.querySelector('.user-profile__name');
+const userProfileProfession = document.querySelector('.user-profile__profession');
+//Аватар
+const userAvatar = document.querySelector('.user-profile__photo-mask');
+//Попап для изменения аватара пользователя
+const popupEditAvatar = document.querySelector('.popup_type_avatar');
+const avatarForm = popupEditAvatar.querySelector('.form_type_avatar');
+const avatarInput = popupEditAvatar.querySelector('#avatar');
+const avatarPhoto = document.querySelector('.user-profile__photo');
+const buttonCloseAvatarForm = popupEditAvatar.querySelector('.popup__button-close-avatar');
+const buttonSubmitAvatar = popupEditAvatar.querySelector('.form__button-submit_type_avatar');
+
+// Попап для добавления карточек
+const popupAddCard = document.querySelector('.popup_type_add-card');
+const buttonAddCard = document.querySelector('.user-profile__button-add');
+const popupFormAddCard = popupAddCard.querySelector('.form_type_add-card');
+const cardNameInput = popupFormAddCard.querySelector('.form__input_type_card-name');
+const cardAboutInput = popupFormAddCard.querySelector('.form__input_type_link');
+const buttonClosePopupAddCard = popupAddCard.querySelector('.popup__button-close_type_card');
+const buttonSubmitCard = document.querySelector('.form__button-submit_type_card');
+// Новые карточки
+const cardTemplate = document.querySelector('.card-element').content;
+
+// Блок, в который будут добавлены новые карточки
+const cardList = document.querySelector('.cards');
+
+// Попап с изображением
+const popupImageContainer = document.querySelector('.popup_type_image');
+const popupImageItem = popupImageContainer.querySelector('.popup__image');
+const popuImageName = popupImageContainer.querySelector('.popup__image-name');
+const buttonClosePopupImage = popupImageContainer.querySelector('.popup__button-close_type_image');
+
+
+export {config};
+export {
+  popupAddCard,
+  buttonAddCard,
+  popupFormAddCard,
+  cardNameInput,
+  cardAboutInput,
+  buttonClosePopupAddCard,
+  buttonSubmitCard,
+  cardTemplate,
+  cardList,
+  popupImageContainer,
+  popupImageItem,
+  popuImageName,
+  buttonClosePopupImage,
+};
+
+export {
+  popupList,
+  popupEditProfile,
+  buttonEditForm,
+  buttonClosePopupEditForm,
+  popupProfileForm,
+  nameInput,
+  jobInput,
+  userProfileName,
+  userProfileProfession,
+  userAvatar,
+  popupEditAvatar,
+  avatarForm,
+  avatarInput,
+  avatarPhoto,
+  buttonCloseAvatarForm,
+  buttonSubmitProfile,
+  buttonSubmitAvatar
+};
--- src/components/utils.js
+++ src/components/utils.js
@@ -1,22 +1,7 @@
-export {openPopup, closePopup};
-
-// Функция для открытия попапов
-function openPopup(popup) {
-  popup.classList.add('popup_opened');
-  document.addEventListener('keydown', handleEscClose);
-}
-
-// Функция для закрытия всех трех попапов
-function closePopup(popup) {
-  popup.classList.remove('popup_opened');
-  document.removeEventListener('keydown', handleEscClose);
-}
-
-//Закрытие попапа нажатием на клавишу esc
-function handleEscClose(evt) {
-  if (evt.key === 'Escape') {
-    evt.preventDefault();
-    const popupisOpened = document.querySelector('.popup_opened');
-    closePopup(popupisOpened);
+export const renderLoading = (submitButtonElement, loadingStatus) => {
+  if (loadingStatus) {
+    submitButtonElement.textContent = 'Сохранение...';
+  } else {
+    submitButtonElement.textContent = 'Сохранить';
   }
-};
+}
