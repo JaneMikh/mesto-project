4b0f45a9c84e858d7b1a574ed8df65164e04eae7
Grbchk <olya-shch@ya.ru>
2022-01-14 20:44:12 +0500

    add class Api

--- src/components/api.js
+++ src/components/api.js
@@ -1,118 +1,252 @@
-const apiConfig = {
-  baseUrl: 'https://mesto.nomoreparties.co/v1/plus-cohort-4', //идентификатор группы plus-cohort-4
-  headers: {
-    Authorization: '53f5a902-2507-4ae9-b8bd-13e370d56b23', //токен
-    'Content-Type': 'application/json'
+
+// const apiConfig = {
+//   baseUrl: 'https://mesto.nomoreparties.co/v1/plus-cohort-4', //идентификатор группы plus-cohort-4
+//   headers: {
+//     Authorization: '53f5a902-2507-4ae9-b8bd-13e370d56b23', //токен
+//     'Content-Type': 'application/json'
+//   }
+// }
+
+
+//больше не экспортируем ничего
+export default class Api {
+  constructor(options) {  //сюда передать конфиг
+    this._url = options.baseUrl;
+    this._headers = options.headers;
   }
-}
 
-//получить информацию о пользователе c сервера
-const getUserInfo = () => {
-  return fetch(`${apiConfig.baseUrl}/users/me`, {
-    method: 'GET',
-    headers: apiConfig.headers
-  })
-.then(parseResponse)
-}
-
-//проверка работы сервера
-const parseResponse = (res) => {
-  if (res.ok) {
-    return res.json();
+  //проверка работы сервера
+  _parseResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
   }
-  return Promise.reject(`Ошибка: ${res.status}`);
-}
 
-//Отправить запрос на изменение аватара на сервер
-const setUserAvatar = (userAvatar) => {
-  return fetch(`${apiConfig.baseUrl}/users/me/avatar`, {
-    method: 'PATCH',
-    headers: apiConfig.headers,
-    body: JSON.stringify({
-      avatar: userAvatar
+  //получить информацию о пользователе c сервера
+  //Работает console.log(api.getUserInfo());
+  getUserInfo() {
+    return fetch(`${this._url}/users/me`, {
+      method: 'GET',
+      headers: this._headers
     })
-  })
-  .then(parseResponse)
-}
+  .then(this._parseResponse)
+  }
 
-//Отправить запрос на изменение имени и описания на сервер
-const setUserInfo = (userName, userAbout) => {
-  return fetch(`${apiConfig.baseUrl}/users/me`, {
-    method: 'PATCH',
-    headers: apiConfig.headers,
-    body: JSON.stringify({
-      name: userName,
-      about: userAbout
+  //Отправить запрос на изменение аватара на сервер
+  //Работает console.log(api.setUserAvatar('https://krasivosti.pro/uploads/posts/2021-04/1617819813_10-p-koshka-oboi-malenkaya-koshechka-12.jpg'));
+  setUserAvatar(userAvatar) {
+    return fetch(`${this._url}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        userAvatar,
+      })
     })
-  })
-  .then(parseResponse)
-}
+    .then(this._parseResponse)
+  }
 
-//Получить информацию по всем карточкам
-const getCardsInfo = () => {
-  return fetch(`${apiConfig.baseUrl}/cards`, {
-    method: 'GET',
-    headers: apiConfig.headers
-})
-.then(parseResponse)
-}
-
-//Выполнение одновременной загрузки информации о пользователе и карточках
-const getUserCardsInfo = () => {
-  return Promise.all([getUserInfo(), getCardsInfo()]);
-}
-
-//Добавить новую карточку
-const addUserCard = (cardName, cardLink) => {
-  return fetch(`${apiConfig.baseUrl}/cards`, {
-    method: 'POST',
-    headers: apiConfig.headers,
-    body: JSON.stringify({
-      name: cardName,
-      link: cardLink
+  //Отправить запрос на изменение имени и описания на сервер
+  //Работает console.log(api.setUserInfo('Eva', 'Teacher'));
+  setUserInfo(userName, userAbout) {
+    return fetch(`${this._url}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: userName,
+        about: userAbout
+      })
     })
-  })
-  .then(parseResponse)
-}
+    .then(this._parseResponse)
+  }
 
-//Запрос на удаление карточки
-const deleteUserCard = (cardId) => {
-  return fetch(`${apiConfig.baseUrl}/cards/${cardId}`, {
-    method: 'DELETE',
-    headers: apiConfig.headers,
-    body: JSON.stringify({
-      _id: cardId
+  //Получить информацию по всем карточкам
+  //Работает console.log(api.getCardsInfo());
+  getCardsInfo() {
+    return fetch(`${this._url}/cards`, {
+      method: 'GET',
+      headers: this._headers
     })
-  })
-  .then(parseResponse)
+    .then(this._parseResponse)
+  }
+
+  //Добавить новую карточку
+  //Работает console.log(api.addUserCard('Котя','https://krasivosti.pro/uploads/posts/2021-04/1617819813_10-p-koshka-oboi-malenkaya-koshechka-12.jpg'));
+  addUserCard (cardName, cardLink) {
+    return fetch(`${this._url}/cards`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: cardName,
+        link: cardLink
+      })
+    })
+    .then(this._parseResponse)
+  }
+
+  //Запрос на удаление карточки
+  //Работает console.log(api.deleteUserCard('61d75085842a2d0012423d26'));
+  deleteUserCard(cardId) {
+    return fetch(`${this._url}/cards/${cardId}`, {
+      method: 'DELETE',
+      headers: this._headers,
+      body: JSON.stringify({
+        _id: cardId
+      })
+    })
+    .then(this._parseResponse)
+  }
+
+  //Запрос на добавление лайка
+  //Работает console.log(api.setUserLike('61d81243842a2d0012439c98'));
+  setUserLike(cardId) {
+    return fetch(`${this._url}/cards/likes/${cardId}`, {
+      method: 'PUT',
+      headers: this._headers,
+    })
+    .then(this._parseResponse)
+  }
+
+  //Запрос на удаление лайка
+  //Работает console.log(api.deleteUserLike('61d81243842a2d0012439c98'));
+  deleteUserLike(cardId) {
+    return fetch(`${this._url}/cards/likes/${cardId}`, {
+      method: 'DELETE',
+      headers: this._headers,
+    })
+    .then(this._parseResponse)
+  }
+
+  //Выполнение одновременной загрузки информации о пользователе и карточках
+  //Работает console.log(api.getUserCardsInfo());
+  getUserCardsInfo() {
+    return Promise.all([this.getUserInfo(), this.getCardsInfo()]);
+  }
+
 }
 
-//Запрос на добавление лайка
-const setUserLike = (cardId) => {
-  return fetch(`${apiConfig.baseUrl}/cards/likes/${cardId}`, {
-    method: 'PUT',
-    headers: apiConfig.headers,
-  })
-  .then(parseResponse)
-}
 
-//Запрос на удаление лайка
-const deleteUserLike = (cardId) => {
-  return fetch(`${apiConfig.baseUrl}/cards/likes/${cardId}`, {
-    method: 'DELETE',
-    headers: apiConfig.headers,
-  })
-  .then(parseResponse)
-}
 
-export {
-  getUserInfo,
-  setUserInfo,
-  getCardsInfo,
-  addUserCard,
-  deleteUserCard,
-  setUserLike,
-  deleteUserLike,
-  getUserCardsInfo,
-  setUserAvatar
-};
+
+
+
+
+
+// const apiConfig = {
+//   baseUrl: 'https://mesto.nomoreparties.co/v1/plus-cohort-4', //идентификатор группы plus-cohort-4
+//   headers: {
+//     Authorization: '53f5a902-2507-4ae9-b8bd-13e370d56b23', //токен
+//     'Content-Type': 'application/json'
+//   }
+// }
+
+// //получить информацию о пользователе c сервера
+// const getUserInfo = () => {
+//   return fetch(`${apiConfig.baseUrl}/users/me`, {
+//     method: 'GET',
+//     headers: apiConfig.headers
+//   })
+// .then(parseResponse)
+// }
+
+// //проверка работы сервера
+// const parseResponse = (res) => {
+//   if (res.ok) {
+//     return res.json();
+//   }
+//   return Promise.reject(`Ошибка: ${res.status}`);
+// }
+
+// //Отправить запрос на изменение аватара на сервер
+// const setUserAvatar = (userAvatar) => {
+//   return fetch(`${apiConfig.baseUrl}/users/me/avatar`, {
+//     method: 'PATCH',
+//     headers: apiConfig.headers,
+//     body: JSON.stringify({
+//       avatar: userAvatar
+//     })
+//   })
+//   .then(parseResponse)
+// }
+
+// //Отправить запрос на изменение имени и описания на сервер
+// const setUserInfo = (userName, userAbout) => {
+//   return fetch(`${apiConfig.baseUrl}/users/me`, {
+//     method: 'PATCH',
+//     headers: apiConfig.headers,
+//     body: JSON.stringify({
+//       name: userName,
+//       about: userAbout
+//     })
+//   })
+//   .then(parseResponse)
+// }
+
+// //Получить информацию по всем карточкам
+// const getCardsInfo = () => {
+//   return fetch(`${apiConfig.baseUrl}/cards`, {
+//     method: 'GET',
+//     headers: apiConfig.headers
+// })
+// .then(parseResponse)
+// }
+
+// //Выполнение одновременной загрузки информации о пользователе и карточках
+// const getUserCardsInfo = () => {
+//   return Promise.all([getUserInfo(), getCardsInfo()]);
+// }
+
+// //Добавить новую карточку
+// const addUserCard = (cardName, cardLink) => {
+//   return fetch(`${apiConfig.baseUrl}/cards`, {
+//     method: 'POST',
+//     headers: apiConfig.headers,
+//     body: JSON.stringify({
+//       name: cardName,
+//       link: cardLink
+//     })
+//   })
+//   .then(parseResponse)
+// }
+
+// //Запрос на удаление карточки
+// const deleteUserCard = (cardId) => {
+//   return fetch(`${apiConfig.baseUrl}/cards/${cardId}`, {
+//     method: 'DELETE',
+//     headers: apiConfig.headers,
+//     body: JSON.stringify({
+//       _id: cardId
+//     })
+//   })
+//   .then(parseResponse)
+// }
+
+// //Запрос на добавление лайка
+// const setUserLike = (cardId) => {
+//   return fetch(`${apiConfig.baseUrl}/cards/likes/${cardId}`, {
+//     method: 'PUT',
+//     headers: apiConfig.headers,
+//   })
+//   .then(parseResponse)
+// }
+
+// //Запрос на удаление лайка
+// const deleteUserLike = (cardId) => {
+//   return fetch(`${apiConfig.baseUrl}/cards/likes/${cardId}`, {
+//     method: 'DELETE',
+//     headers: apiConfig.headers,
+//   })
+//   .then(parseResponse)
+// }
+
+// export {
+//   getUserInfo,
+//   setUserInfo,
+//   getCardsInfo,
+//   addUserCard,
+//   deleteUserCard,
+//   setUserLike,
+//   deleteUserLike,
+//   getUserCardsInfo,
+//   setUserAvatar
+// };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import './index.css';
 import {renderLoading} from '../components/utils.js'
-import {setUserInfo, addUserCard, getUserCardsInfo, setUserAvatar} from '../components/api.js';
+import Api from '../components/Api.js';
+// import {setUserInfo, addUserCard, getUserCardsInfo, setUserAvatar} from '../components/api.js';
 import {enableValidation, removeAllErrors, enableSubmitButton, disableSubmitButton} from '../components/validate.js';
 import {config} from '../components/constants.js';
 import {openPopup, closePopup, closePopupByOvelay, closePopupByCloseButton} from '../components/modal.js';
@@ -30,106 +31,140 @@ import {
 
 let userId; //id пользователя
 
-closePopupByOvelay();
-closePopupByCloseButton();
-
-getUserCardsInfo()
-.then(([userData, cardsData]) => {
-  userId = userData._id;
-  userProfileName.textContent = userData.name;
-  userProfileProfession.textContent = userData.about;
-  avatarPhoto.src = userData.avatar;
-  cardsData.reverse().forEach(function(el) {
-    let item;
-    el.likes.forEach(function(like) {
-      if (like._id === userId) {
-        return item = true;
-      }
-      return item;
-    })
-    addCard(el.name, el.link, el._id, el.likes.length, el.owner._id, userId, item);
-  });
-})
-.catch((err) => {
-  console.log(err);
-})
-
-//Обработчик отправки формы для создания новой карточки
-function handleCardFormSubmit (evt) {
-  evt.preventDefault();
-  renderLoading(buttonSubmitCard, true);
-  addUserCard(cardNameInput.value, cardAboutInput.value)
-  .then((data) => {
-    addCard(data.name, data.link, data._id, data.likes.length, data.owner._id, userId);
-    closePopup(popupAddCard);
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-  .finally(() => {
-    renderLoading(buttonSubmitCard, false);
-  });
-};
-popupFormAddCard.addEventListener('submit', handleCardFormSubmit);
-
-//Обработчик отправки формы профиля пользователя
-popupProfileForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  renderLoading(buttonSubmitProfile, true);
-  setUserInfo(nameInput.value, jobInput.value)
-  .then((data) => {
-    userProfileName.textContent = data.name;
-    userProfileProfession.textContent = data.about;
-    closePopup(popupEditProfile);
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-  .finally(() => {
-    renderLoading(buttonSubmitProfile, false);
-  });
+//---(Олин конфиг)---
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/plus-cohort-4', //идентификатор группы plus-cohort-4
+  headers: {
+    Authorization: '001ad720-6ef4-4b0d-b9d3-4ac6fa30aca0', //токен
+    'Content-Type': 'application/json'
+  }
 });
 
-//Слушатель отправки формы для изменения аватара пользователя
-avatarForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  renderLoading(buttonSubmitAvatar, true);
-  setUserAvatar(avatarInput.value)
-  .then((data) => {
-    avatarPhoto.src = data.avatar;
-    closePopup(popupEditAvatar);
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-  .finally(() => {
-    renderLoading(buttonSubmitAvatar, false);
-  });
-});
+// api.setUserInfo(item)
+// .then((data) => {...
 
-// Открытие попапа для редактирования профиля пользователя
-buttonEditForm.addEventListener('click', () => {
-  removeAllErrors(popupProfileForm);
-  nameInput.value = userProfileName.textContent;
-  jobInput.value = userProfileProfession.textContent;
-  enableSubmitButton(popupProfileForm);
-  openPopup(popupEditProfile);
-});
+// api.setUserAvatar(item)
+// .then((data) => {...
 
-// Открытие попапа для добавления карточек
-buttonAddCard.addEventListener('click', () => {
-  popupFormAddCard.reset();
-  removeAllErrors(popupFormAddCard);
-  disableSubmitButton(popupFormAddCard);
-  openPopup(popupAddCard);
-});
+// api.addUserCard(item)
+// .then((cardData) => {...
 
-//Открытие попапа изменения аватара
-buttonUserAvatar.addEventListener('click', () => {
-  avatarForm.reset();
-  removeAllErrors(avatarForm);
-  disableSubmitButton(avatarForm);
-  openPopup(popupEditAvatar);
-});
+// api.getUserCardsInfo()
+// .then(([userData, cardsData]) => {
+//еще удаление???
 
-enableValidation(config);
+
+
+
+// //---(Женин конфиг)---
+// const api = new Api({
+//   baseUrl: 'https://mesto.nomoreparties.co/v1/plus-cohort-4', //идентификатор группы plus-cohort-4
+//   headers: {
+//     Authorization: '53f5a902-2507-4ae9-b8bd-13e370d56b23', //токен
+//     'Content-Type': 'application/json'
+//   }
+// });
+
+// closePopupByOvelay();
+// closePopupByCloseButton();
+
+// getUserCardsInfo()
+// .then(([userData, cardsData]) => {
+//   userId = userData._id;
+//   userProfileName.textContent = userData.name;
+//   userProfileProfession.textContent = userData.about;
+//   avatarPhoto.src = userData.avatar;
+//   cardsData.reverse().forEach(function(el) {
+//     let item;
+//     el.likes.forEach(function(like) {
+//       if (like._id === userId) {
+//         return item = true;
+//       }
+//       return item;
+//     })
+//     addCard(el.name, el.link, el._id, el.likes.length, el.owner._id, userId, item);
+//   });
+// })
+// .catch((err) => {
+//   console.log(err);
+// })
+
+// //Обработчик отправки формы для создания новой карточки
+// function handleCardFormSubmit (evt) {
+//   evt.preventDefault();
+//   renderLoading(buttonSubmitCard, true);
+//   addUserCard(cardNameInput.value, cardAboutInput.value)
+//   .then((data) => {
+//     addCard(data.name, data.link, data._id, data.likes.length, data.owner._id, userId);
+//     closePopup(popupAddCard);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   })
+//   .finally(() => {
+//     renderLoading(buttonSubmitCard, false);
+//   });
+// };
+// popupFormAddCard.addEventListener('submit', handleCardFormSubmit);
+
+// //Обработчик отправки формы профиля пользователя
+// popupProfileForm.addEventListener('submit', (evt) => {
+//   evt.preventDefault();
+//   renderLoading(buttonSubmitProfile, true);
+//   setUserInfo(nameInput.value, jobInput.value)
+//   .then((data) => {
+//     userProfileName.textContent = data.name;
+//     userProfileProfession.textContent = data.about;
+//     closePopup(popupEditProfile);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   })
+//   .finally(() => {
+//     renderLoading(buttonSubmitProfile, false);
+//   });
+// });
+
+// //Слушатель отправки формы для изменения аватара пользователя
+// avatarForm.addEventListener('submit', (evt) => {
+//   evt.preventDefault();
+//   renderLoading(buttonSubmitAvatar, true);
+//   setUserAvatar(avatarInput.value)
+//   .then((data) => {
+//     avatarPhoto.src = data.avatar;
+//     closePopup(popupEditAvatar);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   })
+//   .finally(() => {
+//     renderLoading(buttonSubmitAvatar, false);
+//   });
+// });
+
+// // Открытие попапа для редактирования профиля пользователя
+// buttonEditForm.addEventListener('click', () => {
+//   removeAllErrors(popupProfileForm);
+//   nameInput.value = userProfileName.textContent;
+//   jobInput.value = userProfileProfession.textContent;
+//   enableSubmitButton(popupProfileForm);
+//   openPopup(popupEditProfile);
+// });
+
+// // Открытие попапа для добавления карточек
+// buttonAddCard.addEventListener('click', () => {
+//   popupFormAddCard.reset();
+//   removeAllErrors(popupFormAddCard);
+//   disableSubmitButton(popupFormAddCard);
+//   openPopup(popupAddCard);
+// });
+
+// //Открытие попапа изменения аватара
+// buttonUserAvatar.addEventListener('click', () => {
+//   avatarForm.reset();
+//   removeAllErrors(avatarForm);
+//   disableSubmitButton(avatarForm);
+//   openPopup(popupEditAvatar);
+// });
+
+// enableValidation(config);
